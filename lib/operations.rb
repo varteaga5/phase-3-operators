89b99f48e3252dd9ffd616cd5e56ecbcb24487c1
vincent <vincent.arteaga@gmail.com>
2021-07-20 19:20:36 -0700

    commit tree 596a1cdefc766e687b9b715c9955396819e828bf

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,28 @@
 def unsafe?(speed)
-
+    if speed > 60 
+         return true
+    end 
+    # returns true if speed is less than 40 (FAILED - 1)
+    if speed < 40
+        return true
+    end
+    # returns false if the speed is between 40 and 60 (FAILED - 2)
+    if speed > 40 && speed < 60
+        return false
+    end
 end
 
 
 
 def not_safe?(speed)
-	
+    # returns true if speed is greater than 60 (FAILED - 3)
+    speed > 60 ? true : false
+    # returns true if speed is less than 40 (FAILED - 4)
+    speed < 40 ?  true :  false
+    # returns false if the speed is between 40 and 60 (FAILED - 5)
+    speed > 40 && speed < 60 ? false: true  
+    # uses the ternary operator (FAILED - 6)
+
 end
 	
 
